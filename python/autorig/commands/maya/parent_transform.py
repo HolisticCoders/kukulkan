7345ed0c1bf32b099697998183d95fdc6aaf5b31
muream <muream@gmail.com>
2017-09-10 23:26:41 +0200

    allow for a string to be passed for parent argument and fix a syntax error

--- python/autorig/commands/maya/parent_transform.py
+++ python/autorig/commands/maya/parent_transform.py
@@ -2,21 +2,24 @@ import maya.api.OpenMaya as om
 
 import get_mobject
 
+
 def run(child, parent=None, *args, **kwargs):
     """Parent an object to another
 
     :param child: MObject of the child node.
     :type child: MObject or str
     :param parent: MObject of the parent node.
-    :type parent: MObject or None
+    :type parent: MObject or str or None
     """
     dag_mod = om.MDagModifier()
 
     if parent is None:
         parent = om.MObject.kNullObj
+    elif isinstance(parent, str):
+        parent = get_mobject.run(parent)
 
     if isinstance(child, str):
         child = get_mobject.run(child)
 
     dag_mod.reparentNode(child, newParent=parent)
-    dag.doIt()
+    dag_mod.doIt()
