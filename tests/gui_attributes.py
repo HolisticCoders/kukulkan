bfab4049d1c741ffabe19883cc117c03825ed9f6
Muream <muream@gmail.com>
2017-09-26 10:31:27 +0200

    one attribute per node

--- tests/gui_attributes.py
+++ tests/gui_attributes.py
@@ -19,18 +19,17 @@ def main():
     app = _qt.QApplication(sys.argv)
 
     window = kukulkan.gui.api.window.GraphWindow()
-    node1 = window.scene.add_node('Floats')
-    node2 = window.scene.add_node('Ints')
-    node3 = window.scene.add_node('Bools')
-    node4 = window.scene.add_node('Strings')
-    node5 = window.scene.add_node('Enums')
+    node1 = window.scene.add_node('Float')
+    node2 = window.scene.add_node('Int')
+    node3 = window.scene.add_node('Bool')
+    node4 = window.scene.add_node('String')
+    node5 = window.scene.add_node('Enum')
 
-    for i in range(25):
-        node1.add_attribute('FloatAttr' + str(i), _attributes.FloatAttribute)
-        node2.add_attribute('IntAttr' + str(i), _attributes.IntAttribute)
-        node3.add_attribute('BoolAttr' + str(i), _attributes.BoolAttribute)
-        node4.add_attribute('StringAttr' + str(i), _attributes.StringAttribute)
-        node5.add_attribute('EnumAttr' + str(i), _attributes.EnumAttribute)
+    node1.add_attribute('FloatAttr', _attributes.FloatAttribute)
+    node2.add_attribute('IntAttr', _attributes.IntAttribute)
+    node3.add_attribute('BoolAttr', _attributes.BoolAttribute)
+    node4.add_attribute('StringAttr', _attributes.StringAttribute)
+    node5.add_attribute('EnumAttr', _attributes.EnumAttribute)
     window.show()
 
     sys.exit(app.exec_())
