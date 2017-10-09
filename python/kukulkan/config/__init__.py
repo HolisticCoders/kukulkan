790bc0aaa809d121350a070a43bb3ff4c20fc7a0
Aphosis <alexy.long@orange.fr>
2017-10-09 21:45:12 +0200

    Automatically create ConfigReader and FolderWatcher objects.

--- python/kukulkan/config/__init__.py
+++ python/kukulkan/config/__init__.py
@@ -1,6 +1,30 @@
+import os
+
+from .collector import get_default_folder
 from .reader import ConfigReader
+from .watchers import FolderWatcher
 
 
-UI = ConfigReader('ui')
-# PIPE = ConfigReader('pipe')
-# KEYMAP = ConfigReader('keymap')
+def setup():
+    """Setup Kukulkan configuration.
+
+    Gather all configuration files and associate a `FileWatcher`.
+    Same for folders, with `FolderWatcher` objects.
+    """
+    root = get_default_folder()
+    _walk_folder(root)
+
+
+def _walk_folder(root):
+    for name in os.listdir(root):
+        path = os.path.join(root, name)
+        if os.path.isfile(path):
+            key = name.split('.')[0]
+            reader = ConfigReader(key)
+            globals()[key.upper()] = reader
+        elif os.path.isdir(path):
+            kukulkan.config.watchers.FolderWatcher(name, path)
+            _walk_folder(path)
+
+
+setup()
