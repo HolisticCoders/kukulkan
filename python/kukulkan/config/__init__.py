21f4ff3035216cd7de1edcc2f361b4d0e262d092
Aphosis <alexy.long@orange.fr>
2017-10-09 21:58:27 +0200

    Removed recursive behavior

--- python/kukulkan/config/__init__.py
+++ python/kukulkan/config/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from .collector import get_default_folder
+from .collector import get_default_folder, get_configuration_folder_choices
 from .reader import ConfigReader
 from .watchers import FolderWatcher
 
@@ -12,19 +12,23 @@ def setup():
     Same for folders, with `FolderWatcher` objects.
     """
     root = get_default_folder()
-    _walk_folder(root)
-
-
-def _walk_folder(root):
     for name in os.listdir(root):
         path = os.path.join(root, name)
         if os.path.isfile(path):
             key = name.split('.')[0]
             reader = ConfigReader(key)
             globals()[key.upper()] = reader
+            print 'New ConfigReader:', key
         elif os.path.isdir(path):
-            kukulkan.config.watchers.FolderWatcher(name, path)
-            _walk_folder(path)
+            FolderWatcher(name, path)
+            print 'New FolderWatcher:', name
+
+            choices = get_configuration_folder_choices(name, candidate=True)
+            for choice in choices:
+                key = choice.split('.')[0]
+                reader = ConfigReader(key, folder=name)
+                globals()[key.upper()] = reader
+                print 'New ConfigReader:', key, name
 
 
 setup()
