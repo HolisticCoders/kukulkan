d76ee113220cb7f5f916af8669c4f33797102518
Aphosis <alexy.long@orange.fr>
2017-10-09 21:07:15 +0200

    Added FolderWatcher class

--- python/kukulkan/config/watchers.py
+++ python/kukulkan/config/watchers.py
@@ -61,3 +61,100 @@ class FileWatcher(threading.Thread):
         """Called when the watched file changes."""
         kukulkan.events.notify('config.' + self.name + '.changed')
 
+
+class FolderWatcher(threading.Thread):
+    """Watch for folder changes.
+
+    Also, update the damn application when that happens.
+
+    This watcher notifies watched folder path creation, deletion, and
+    child creation and deletion.
+
+    :param name: Name of this `FolderWatcher`.
+    :param path: Path to observe.
+    :type name: str
+    :type path: str
+    """
+
+    def __init__(self, name, path):
+        super(FolderWatcher, self).__init__()
+        self.name = name
+        self.path = path
+        if not os.path.isdir(path):
+            self.exists = False
+            self.children = set()
+        else:
+            self.exists = True
+            self.children = set(os.listdir(self.path))
+
+    def run(self):
+        """Update the state of the file."""
+        while True:
+            if self.exists:
+                if not os.path.isdir(self.path):
+                    # Does not exist anymore.
+                    self.exists = False
+                    self.folder_deleted()
+            else:
+                if os.path.isdir(self.path):
+                    # Just got created.
+                    self.exists = True
+                    self.children = os.listdir(self.path)
+                    self.folder_created()
+
+            old_children = self.children
+            if os.path.isdir(self.path):
+                self.children = set(os.listdir(self.path))
+            else:
+                self.children = set()
+
+            created_children = self.children - old_children
+            deleted_children = old_children - self.children
+
+            for child in created_children:
+                self.child_created(child)
+
+            for child in deleted_children:
+                self.child_deleted(child)
+
+            time.sleep(1)
+
+    def _event_key(self):
+        """Return the watched folder event key.
+
+        :rtype: str
+        """
+        return 'config.' + self.name
+
+    def _child_event_key(self, name):
+        """Return the watched child event key.
+
+        :rtype: str
+        """
+        return 'config.' + '.'.join([self.name, name])
+
+    def folder_deleted(self):
+        """Called when the watched folder gets deleted."""
+        kukulkan.events.notify(self._event_key() + '.deleted')
+
+    def folder_created(self):
+        """Called when the watched folder is created."""
+        kukulkan.events.notify(self._event_key() + '.created')
+
+    def child_deleted(self, name):
+        """Called when a child file or folder gets deleted.
+
+        Notification sends the created child base name to subscribers.
+
+        :param str name: Name of the deleted child.
+        """
+        kukulkan.events.notify(self._child_event_key(name) + '.deleted')
+
+    def child_created(self, name):
+        """Called when a child file or folder is created.
+
+        Notification sends the created child base name to subscribers.
+
+        :param str name: Name of the created child.
+        """
+        kukulkan.events.notify(self._child_event_key(name) + '.created')
